a95fee72f7d7b40589154806bc43bfbc20942fa0
Kevin Van Damme <kevin.van.damme.pxl@protonmail.com>
2025-06-03 14:08:44 +0200

    add

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,11 +5,15 @@ import { drupal } from "@/lib/drupal";
 const page = async () => {
   const res = await fetch("https://be-buitenbijons-v2.ddev.site/jsonapi/node/article");
   const data = await res.json();
-  const params = new DrupalJsonApiParams().addFields("node--article", ["title", "body"]).addFilter("status", "1").addSort("created", "DESC");
+  const params = new DrupalJsonApiParams()
+    .addFields("node--article", ["title", "body", "field_image"])
+    .addFilter("status", "1")
+    .addSort("created", "DESC");
   console.log(params);
+
   const nodes = await drupal.getResourceCollection<DrupalNode[]>("node--article", {
     params: {
-      include: "body",
+      include: "field_image",
     },
   });
 
@@ -17,9 +21,9 @@ const page = async () => {
     <>
       {nodes && nodes.length > 0 ? (
         nodes.map((article) => (
-          <div>
-            <h3>{article.id}</h3>
-            {/* <p>{article.data.attributes.body}</p> */}
+          <div key={article.id}>
+            <h3>{article.title}</h3>
+            <p>{article.body}</p>
           </div>
         ))
       ) : (
